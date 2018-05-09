94fc87c1a4eba4b5cc3cdcf3a1c279e4f04c7552
Andrew Frick <ajfrick71@gmail.com>
2018-05-09 16:59:47 -0700

    Added Functionality for extract_Yr and extract_Mo to return Date as numeric instead of Date object.

--- R/extract_Mo.R
+++ R/extract_Mo.R
@@ -6,9 +6,13 @@
 #' @examples
 #' extract_Mo(Sys.Date())
 #' extract_Mo("1989-03-12")
+#' extract_Mo("1989-03-12",numeric = T)
 #' extract_Mo("031289", format = "%m%d%y")
+#' extract_Mo("031289", format = "%m%d%y", numeric = T)
 #' extract_Mo("12_1989_03", format = "%d_%Y_%m")
 
-extract_Mo = function(x,format = "%Y-%m-%d"){
-  format(as.Date(x, format= format),"%m")
+extract_Mo = function(x,format = "%Y-%m-%d", numeric = F){
+  d = format(as.Date(x, format= format),"%m")
+  if(numeric){return(as.numeric(d))}
+  d
 }
--- R/extract_Yr.R
+++ R/extract_Yr.R
@@ -8,7 +8,9 @@
 #' extract_Yr(Sys.Date())
 #' extract_Yr("1989-03-12")
 #' extract_Yr("031289", format = "%m%d%y")
+#' extract_Yr("031289", format = "%m%d%y", numeric = T)
 #' extract_Yr("12_1989_03", format = "%d_%Y_%m")
+#' extract_Yr("12_1989_03", format = "%d_%Y_%m", numeric = T)
 
 extract_Yr = function(x,format = "%Y-%m-%d", numeric = F){
   d = format(as.Date(x, format= format),"%Y")
