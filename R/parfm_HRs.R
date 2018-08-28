5f0be64705040e5e0d1e78822fe38ff60f247ec9
Andrew Frick <ajfrick71@gmail.com>
2018-08-28 15:57:31 -0700

    parfm_HRs function added

---
+++ R/parfm_HRs.R
@@ -0,0 +1,29 @@
+#' Generate Hazard ratio, CI,  Pval table from parfm
+#'
+#' @param fit model fit by parfm function
+#' @param pred Optional Argument for models with single predictor
+#' @description Generates table with Hazard rate, 95 CI and pval for parfm models fixed effects
+#' WIP name extraction via other methods, until then names for models with single predictor
+#' will have blank in Name column
+#' @return Tibble with 5 columns: Name (char), HR, lower CI, Upper CI, p-val (doubles)
+#' @examples
+#' data(kidney)
+#'
+#' fit = parfm(Surv(time,status) ~ age + sex + disease,
+#'              cluster = "id", data = kidney,
+#'              dist = "weibull", frailty = "gamma")
+#'
+#' parfm_HRs(fit)
+
+parfm_HRs = function(fit, pred = NA){
+  CI  = ci.parfm(fit)
+  tab = dplyr::tibble(
+    Name= ifelse(is.null(names(coef(fit))),pred,names(coef(fit))),
+    HR = exp(coef(fit)),
+    low = CI[,1],
+    upp = CI[,2],
+    p = na.omit(fit[,3])
+  )
+  tab
+}
+
